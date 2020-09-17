5a8c84dd29fa654703207a446eb071cc2a8e281d
Thibaut <ThibautHolberton>
2020-09-17 09:54:59 +0000

    print number in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - enter point
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	int integer;
+
+	for (integer = 0; integer < 10; integer++)
+	{
+		printf("%d", integer);
+	}
+	printf("\n");
+	return (0);
+}
