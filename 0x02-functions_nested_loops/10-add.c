df78a616c9640d5073b350506a25ac2a076bdc1c
Thibaut <ThibautHolberton>
2020-09-21 19:02:55 +0000

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * add - add 2 numbers
+ * @first: first integer
+ * @second: second integer
+ * Return: return value adition
+ */
+int add(int first, int second)
+{
+	return (first + second);
+}
