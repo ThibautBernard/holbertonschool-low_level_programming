c7331dd0b4ca7175c6c73e630a114053e646dad7
Thibaut <ThibautHolberton>
2020-09-21 17:09:26 +0000

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+int _putchar(char c);
+
+/**
+ * print_last_digit - return last digit
+ * @y: int checked
+ * Return: last digit
+ */
+int print_last_digit(int y)
+{
+	return (y % 10);
+}
+
