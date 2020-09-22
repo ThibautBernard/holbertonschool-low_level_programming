861200731deb8869fe8426f2d701d0e2be3e1b2e
Thibaut <ThibautHolberton>
2020-09-22 15:29:38 +0000

    try

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,32 +1,6 @@
 #include <stdio.h>
 #include "holberton.h"
 
-void leapYear(int month, int day, int year)
-{
-	if ((month <= 0 || month > 12) && (day < 1 || day > 31))
-	{
-		printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-	}
-	else
-	{
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
-	}
-}
-
-void normalYear(int month, int day, int year)
-{
-	if ((month <= 0 || month > 12) && (day < 1 || day > 31))
-	{
-		printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-	}
-	else
-	{
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 365 - day);
-	}
-}
-
 /**
  * print_remaining_days - takes a date and prints how many days are
  * left in the year, taking leap years into account
@@ -36,28 +10,28 @@ void normalYear(int month, int day, int year)
  * Return: void
  */
 void print_remaining_days(int month, int day, int year)
-{ 
- 
+{
 	if ((year % 4 == 0 && year % 400 == 0) && (year % 100 == 0))
 	{
-		if ((month == 4 || month == 6 || month == 9 || month == 11) && (day == 31))
-		{  
-			printf("Invalid date: %02d/%02d/%04d\n", month, day, year);	
-		}
-		else
+		if (month >= 2 && day >= 60)
 		{
-			leapYear(month, day, year);
+			day++;
 		}
+
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
-		if ((month == 4 || month == 6 || month == 9 || month == 11) && day == 31)
+		if (month == 2 && day == 60)
 		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
-		else	
+		else
 		{
-			normalYear(month, day, year);
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
+
 }
