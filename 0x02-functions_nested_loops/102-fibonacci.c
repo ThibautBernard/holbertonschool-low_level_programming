18274be2d5a31142a880b13441106625c6970c76
Thibaut <ThibautHolberton>
2020-09-22 19:03:32 +0000

    fibo

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -13,13 +13,13 @@ int main(void)
 
 	for (i = 1; i < 50; i++)
 	{
-		printf("%d, ", i + (i-1));
+		printf("%d, ", i + (i - 1));
 	}
 	printf("\n");
 
 	for (y = 2; y < 50; y++)
 	{
-		printf("%d, ", y + (y-1));
+		printf("%d, ", y + (y - 1));
 	}
 
 	printf("\n");
