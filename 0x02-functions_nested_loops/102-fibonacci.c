1a3a2512a3e2c4579389661229262dae240185ff
Thibaut <ThibautHolberton>
2020-09-23 06:47:42 +0000

    fibo

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
-	long int sum;
-	long int lastNumber = 1;
-	long int currentNumber = 1;
+	long long int sum;
+	long long int lastNumber = 1;
+	long long int currentNumber = 1;
 	int i;
 
 	printf("%d, ", 1);
-	for (i = 1; i <= 49; i++)
+	for (i = 1; i <= 50; i++)
 	{
 		sum = lastNumber + currentNumber;
-		printf("%ld, ", sum);
+		printf("%lld, ", sum);
 		lastNumber = currentNumber;
 		currentNumber = sum;
 	}
