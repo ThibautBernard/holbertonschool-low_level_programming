7710462038c1cfbf04c0aeb7f76c9a483a338bd1
Thibaut <ThibautHolberton>
2020-09-23 08:11:51 +0000

    suù

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entry poiunt
+ * Return: sum of multipe of 3 and 5 until 1024
+ */
+int main(void)
+{
+	long int sum;
+	long int lastNumber = 1;
+	long int currentNumber = 1;
+	long int sumtot;
+	int i;
+
+	for (i = 1; i <= 49; i++)
+	{
+		sum = lastNumber + currentNumber;
+		lastNumber = currentNumber;
+		currentNumber = sum;
+		if (!(sum > 4000000))
+		{
+			sumtot += sum;
+		}
+	}
+	printf("%ld", sumtot);
+	printf("\n");
+
+	return (0);
+}
