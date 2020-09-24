42aa447aef250bed6e4639db484d7a65f222c017
Thibaut <ThibautHolberton>
2020-09-24 13:02:07 +0000

    create fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * print 1 to 100 except multiple of 3 & 5 || multiple  3 + 5
+ * Return: number or Fizz || buzz || FizzBuzz
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if ((i % 3 && i % 5) == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+
+
+}
