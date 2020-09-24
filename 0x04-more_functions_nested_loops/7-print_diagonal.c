0609962ffb875a1763291671fabe80ff416c8cfa
Thibaut <ThibautHolberton>
2020-09-24 12:38:55 +0000

    create function & update header

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - print '\' in function of n
+ * @n: integer
+ * Return: pyramid of '\'
+ */
+void print_diagonal(int n)
+{
+	int i, y;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		y = 0;
+		while (y < n)
+		{
+			for (i = 0; i <= y; i++)
+			{
+				_putchar(' ');
+				if (i == y)
+					_putchar('\\');
+			}
+			_putchar('\n');
+			y++;
+		}
+	}
+}
