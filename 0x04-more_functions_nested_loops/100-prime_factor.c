88604e19488829f61a85b547710e654d96e36d5c
Thibaut <ThibautHolberton>
2020-09-24 15:06:19 +0000

    not created but inspired :')

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * prime factor of n
+ * Return: the highest prime factor
+ */
+int main(void)
+{
+	long int i, max, n, isprime;
+
+	n = 612852475143;
+	max = -1;
+	while (n % 2 == 0)
+	{
+		max = 2;
+		n = n / 2;
+	}
+	for (i = 3; i <= n; i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			max = i;
+			n = n / i;
+		}
+	}
+	if (n > 2)
+	{
+		max = n;
+	}
+	printf("%ld\n", max);
+}
