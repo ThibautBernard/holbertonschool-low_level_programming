be1db22128e59c268e93407daa5613b32fd5421b
Thibaut <ThibautHolberton>
2020-09-28 15:49:09 +0000

    feat(puts_half && holberton): create function puts_half && update holberton files header

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * puts_half - print second part of a string if is pair
+ * @str: string given
+ * Return: second part string or last char if impair
+ */
+void puts_half(char *str)
+{
+	int i, y, length;
+
+	for (y = 0; *(str + y) != '\0'; y++)
+		length++;
+	if (length % 2 == 0)
+	{
+		for (i = length / 2; i < length; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		_putchar(str[length - 1]);
+	}
+	_putchar('\n');
+}
+
