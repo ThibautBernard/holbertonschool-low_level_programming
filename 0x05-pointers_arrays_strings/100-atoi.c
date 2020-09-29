f9c4dd74b03dfed448ae2e8ea09a2d8a4adcc0c9
Thibaut <ThibautHolberton>
2020-09-29 16:03:06 +0000

    try to fix this

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -37,7 +37,7 @@ int _atoi(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] >= '0' && s[i] <= '9')
-			test = ((test * 10) + (sign * (s[i] - '0')));
+			test = test * 10 + sign * (s[i] - '0');
 		else if (test > 0 && !(s[i] >= 0 && s[i] <= 9))
 		{
 			return (test);
