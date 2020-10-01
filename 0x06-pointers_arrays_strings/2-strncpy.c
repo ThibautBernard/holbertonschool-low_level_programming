6a5b920be7f2c46ae03da334f17654458b1311a7
Thibaut <ThibautHolberton>
2020-10-01 07:55:00 +0000

    feat(strncpy && holberton): create function strncpy && update holberton header file

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * *_strncpy - copy string to dest in n length
+ * @dest: string to return
+ * @src: string to copy
+ * @n: size to copy
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int y;
+	char *ptr;
+
+	ptr = dest;
+	for (y = 0; src[y] != '\0' && y < n; y++)
+	{
+		*ptr = src[y];
+		ptr++;
+	}
+	for (; y < n; y++)
+	{
+		*ptr = '\0';
+		ptr++;
+	}
+	*ptr = '\0';
+	return (dest);
+}
