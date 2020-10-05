e2a09844dbbce75f81c80bb8aeade6e4076eae2e
Thibaut <ThibautHolberton>
2020-10-05 17:22:01 +0000

    fix

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,6 @@
 #include "holberton.h"
+#include <stdio.h>
+
 /**
  * *_strstr - pointing on word matching with needle
  * @haystack: string pointed to
@@ -7,20 +9,32 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, y;
+	int i = 0, y = 0, m = 0, lenth = 0;
 
-	for (i = 0; needle[i] != '\0'; i++)
+	while (needle[m])
 	{
-		for (y = 0; haystack[y] != '\0'; y++)
+		m++;
+		lenth++;
+	}
+	for (; haystack[i] != '\0';)
+	{
+		for (y = 0; (needle[0] == haystack[i]) && needle[y]; y++)
 		{
-			if (needle[i] == haystack[y])
+			if (needle[y] == haystack[i + y])
 			{
-				if (needle[i + 1] == haystack[y + 1])
-				{
-					haystack = haystack + y;
-					return (haystack);
-				}
+				y++;
 			}
+			else
+				break;
+		}
+		if (y == lenth || (y - 1) == lenth)
+		{
+			return (haystack + i);
+		}
+		else
+		{
+			i++;
+			y = 0;
 		}
 	}
 	return (0);
