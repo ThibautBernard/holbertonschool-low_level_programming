c11aa814fc0a758af96ed73b9fd369a8cf1e02ab
Thibaut <ThibautHolberton>
2020-10-07 15:09:53 +0000

    feat(prime_number && holberton)

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * calcul - calcul the prime number with i
+ * @n: the number
+ * @i: index to % number
+ * Return: 0 if not, 1 otherwise
+ */
+int calcul(int n, int i)
+{
+		if (i == 1)
+			return (1);
+		if (n % i == 0)
+			return (0);
+		return (calcul(n, i - 1));
+}
+/**
+ * is_prime_number - return if is a prime number
+ * @n: integer given
+ * Return: 0 if not prime, 1
+ */
+int is_prime_number(int n)
+{
+		if (n <= 0)
+			return (0);
+		else
+			return (calcul(n, n / 2));
+}
