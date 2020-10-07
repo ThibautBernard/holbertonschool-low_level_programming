cede418e0d575d36eccf628b95e76784f6e2a721
Thibaut <ThibautHolberton>
2020-10-07 10:28:37 +0000

    feat(power && holberton)

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - number pow
+ * @x: number given
+ * @y: power
+ * Return: number
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y != 0)
+	{
+		return (x * _pow_recursion(x,  y - 1));
+	}
+	else if (y < 0)
+		return (-1);
+	return (1);
+}
