f04b87e8f184ffea06e6cf1a113979e47395991a
Thibaut <ThibautHolberton>
2020-10-07 11:02:43 +0000

    feat(print_rev_recursion && holberton)

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _print_rev_recursion - print reverse string recursively
+ * @s: the string given
+ * Return: the string reverse
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+	}
+		_putchar(*s);
+}
