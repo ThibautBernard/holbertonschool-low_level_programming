0122a6f20438f706e2d008f54d381ae07b6a7efe
Thibaut <ThibautHolberton>
2020-10-09 08:31:03 +0000

    feat(argc-argv)

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: value of arguments
+ * Return: (0)
+ */
+int main(int argc, char **argv)
+{
+		if (argc > 0)
+			printf("%s\n", argv[0]);
+		return (0);
+}
