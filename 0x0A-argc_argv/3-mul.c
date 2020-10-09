249f58c211d9c3f073893d29a091c3b5da7e71ec
Thibaut <ThibautHolberton>
2020-10-09 09:21:31 +0000

    add(_atoi)

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,45 @@
 #include <stdio.h>
-#include <stdlib.h>
+/**
+ * minus - count the number of sign (- or + )
+ * @c: char
+ * Return: Return -1 if there is more sign '-'
+ */
+int minus(char *c)
+{
+	int sign = 1, i;
+
+	for (i = 0; c[i] != '\0' ; i++)
+	{
+		if (c[i] == '-')
+			sign *= -1;
+		else if (c[i] >= '0' && c[i] <= '9')
+			break;
+	}
+	return (sign);
+}
+/**
+ * _atoi - convert char to int (only number)
+ * @s: string given
+ * Return: the int number
+ */
+int _atoi(char *s)
+{
+	int i, sign;
+	int test = 0;
+
+	sign = minus(s);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+			test = test * 10 + sign * (s[i] - '0');
+		else if (test > 0 && !(s[i] >= 0 && s[i] <= 9))
+		{
+			return (test);
+		}
+	}
+	return (test);
+}
+
 /**
  * main - entry point
  * @argc: number of arguments
@@ -17,8 +57,8 @@ int main(int argc, char **argv)
 		}
 		else
 		{
-			y = atoi(*(argv + 1));
-			i = atoi(*(argv + 2));
+			y = _atoi(*(argv + 1));
+			i = _atoi(*(argv + 2));
 			printf("%d\n", y * i);
 		}
 		return (0);
