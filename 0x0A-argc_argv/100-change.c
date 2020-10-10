afeecdc56a5bed81af1a6d92afc80935eecae9ca
Thibaut <ThibautHolberton>
2020-10-10 18:15:40 +0000

    fix

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -47,46 +47,39 @@ int _atoi(char *s)
  */
 int main(int argc, char **argv)
 {
-		int i, y, ret, tab[5] = {25, 10, 5, 2, 1};
+		int i, y, sum = 0, ret, tab[5] = {25, 10, 5, 2, 1};
 
 		y = _atoi(*(argv + 1));
+		if (y < 1)
+		{
+			printf("0\n");
+			return (0);
+		}
 		if (argc != 2)
 		{
-				printf("Error \n");
-				return (1);
+			printf("Error \n");
+			return (1);
 		}
-		else if (y < 1)
-			printf("0\n");
 		else
 		{
 			i = 0;
 			while (i < 5)
 			{
-				if (y < tab[i])
+				if (tab[i] > y)
 					i++;
-				else if (y == tab[i])
-				{	
-					if (ret > 0)
-						printf("%d\n", ret);
-					else
-						printf("1\n");
+				else if (tab[i] % y == 0)
+				{
+					sum = sum + y / tab[i];
+					printf("%d\n", sum);
 					return (0);
 				}
-				else if (y % tab[i] != 0 || y % tab[i] == 0)
+				else if (tab[i] % y != 0)
 				{
-					if (y % tab[i] != 0)
-					{
-						ret = y % tab[i];
-						y = y - y % tab[i];
-					}
-					else 
-					{
-						y = y / tab[i];
-						printf("%d\n", y + ret);
-						return (0);
-					}
+					sum = sum + y / tab[i];
+					y = y % tab[i];
 				}
 			}
+			printf("%d\n", sum);
 		}
 		return (0);
 }
