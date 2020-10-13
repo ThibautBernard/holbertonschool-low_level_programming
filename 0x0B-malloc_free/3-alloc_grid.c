ee04a23cdae544804ab731abbc55030b8772b93b
Thibaut <ThibautHolberton>
2020-10-13 10:58:59 +0000

    fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,11 +25,12 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	array = malloc((width + 1) * sizeof(int *));
+	array = malloc(width* sizeof(int *));
 	if (array == NULL)
 		return (NULL);
 	for (i = 0; i < width; i++)
 		array[i] = malloc(height * sizeof(int *));
+	array[i] = '\0';
 	for (i = 0; i < width; i++)
 	{
 		for (y = 0; y < height; y++)
