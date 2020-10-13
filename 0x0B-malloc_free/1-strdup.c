07a5b734d28b31d9315f0668e18532c93dbaef30
Thibaut <ThibautHolberton>
2020-10-13 08:02:20 +0000

    feat(strdup && holberton)

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _strlen - count length of a string
+ * @s: pointer char
+ * Return: the length of the string given
+ */
+int _strlen(char *s)
+{
+	int i, count = 0;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+		count++;
+	return (count);
+}
+/**
+ * *_strdup - copy string str to another and return new string if success
+ * @str: string given
+ * Return: new string
+ */
+char *_strdup(char *str)
+{
+	char *n_string;
+	int i, length_str = 0;
+
+	if (str == NULL)
+		return (0);
+	length_str = _strlen(str);
+	n_string = malloc(length_str * sizeof(char));
+	if (n_string == NULL)
+		return (0);
+	for (i = 0; i < length_str; i++)
+		*(n_string + i) = *(str + i);
+	return (n_string);
+}
