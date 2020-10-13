0390d07eb5aa98688c71855694b6aae72ec056ff
Thibaut <ThibautHolberton>
2020-10-13 12:05:07 +0000

    fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,6 +9,8 @@ int _strlen(char *s)
 {
 	int i, count = 0;
 
+	if (s == NULL)
+		return (0);
 	for (i = 0; *(s + i) != '\0'; i++)
 		count++;
 	return (count);
@@ -25,23 +27,33 @@ char *str_concat(char *s1, char *s2)
 	char *n_string;
 	int i, y, length_s1 = 0, length_s2 = 0;
 
+	if (s1 == NULL || s2 == NULL)
+	{
+		if (s1 == NULL)
+		{
+			length_s1 = 0;
+			length_s2 = _strlen(s2);
+		}
+		if (s2 == NULL)
+		{
+			length_s2 = 0;
+			length_s1 = _strlen(s1);
+		}
+		if (s1 && s2 == NULL)
+		{
+			length_s1 = _strlen(s1);
+			length_s2 = _strlen(s2);
+		}
+	}
 	n_string = malloc((length_s1 + length_s2 + 1) * sizeof(char));
 	if (n_string != NULL)
 	{
-		if (s1 != NULL)
+		for (i = 0; i < length_s1; i++)
+			*(n_string + i) = *(s1 + i);
+		for (y = 0; y < length_s2; y++)
 		{
-			length_s1 = _strlen(s1);
-			for (i = 0; i < length_s1; i++)
-				*(n_string + i) = *(s1 + i);
-		}
-		if (s2 != NULL)
-		{
-			length_s2 = _strlen(s2);
-			for (y = 0; y < length_s2; y++)
-			{
-				*(n_string + i) = *(s2 + y);
-				i++;
-			}
+			*(n_string + i) = *(s2 + y);
+			i++;
 		}
 	*(n_string + i) = '\0';
 	}
