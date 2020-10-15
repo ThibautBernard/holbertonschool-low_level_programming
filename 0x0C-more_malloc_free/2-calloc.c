235f34618bb6490b4c66e7bd151d1ef9b731c9ef
Thibaut <ThibautHolberton>
2020-10-15 09:15:01 +0000

    feat(calloc && holberton)

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * *_calloc - allocate array and fill with 0
+ * @nmemb: length of the array
+ * @size: length of bytes of each element of the array
+ * Return: array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *arr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	arr = malloc(sizeof(size) * nmemb);
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+	{
+		arr[i] = 0;
+	}
+	return (arr);
+}
