be5eb7f1329dd9bb519f672d57e77c49d4addbce
Thibaut <ThibautHolberton>
2020-10-15 10:16:51 +0000

    fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	arr = malloc(size * nmemb);
+	arr = malloc(nmemb * sizeof(size));
 	if (arr == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb; i++)
