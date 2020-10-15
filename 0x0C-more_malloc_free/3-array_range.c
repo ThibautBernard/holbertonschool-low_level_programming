25d83069f2055d0a87b356cf1427cf5c6a4aaa4a
Thibaut <ThibautHolberton>
2020-10-15 11:50:44 +0000

    fix(first condition

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,7 +10,7 @@ int *array_range(int min, int max)
 {
 	int *arr, i, y = 0, size = 0;
 
-	if (min >= max)
+	if (min > max)
 		return (NULL);
 	size = ((max - min) + 2);
 	arr = malloc(sizeof(int) * (size));
