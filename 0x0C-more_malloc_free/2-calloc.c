377843160777f4f93add0cfda73904c0ffe884bc
Thibaut <ThibautHolberton>
2020-10-15 12:13:37 +0000

    fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,25 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
+ * *_memset - fill memory in n range
+ * @s: string pointed to
+ * @b: char constant at replace
+ * @n: number
+ * Return: the memory filled
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i, no;
+
+	no = n;
+	for (i = 0; i < no; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+
+}
+/**
  * *_calloc - allocate array and fill with 0
  * @nmemb: length of the array
  * @size: length of bytes of each element of the array
@@ -8,7 +27,6 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i;
 	void *arr;
 
 	if (nmemb <= 0 || size <= 0)
@@ -16,10 +34,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	arr = malloc(sizeof(size) * (nmemb + 1));
 	if (arr == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
-	{
-		arr[i] = 0;
-	}
-	arr[i] = '\0';
+	_memset(arr, 0, nmemb);
 	return (arr);
 }
