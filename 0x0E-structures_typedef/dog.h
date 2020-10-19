1cfbd0d415a606e7deba455b84f4de0cae073c3c
Thibaut <ThibautHolberton>
2020-10-19 08:27:14 +0000

    feat(): my first struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct dog - struc dog
+ * @name: name of the dog
+ * @age: age
+ * @owner: owner of the dog
+ * Description: a dog structure
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
