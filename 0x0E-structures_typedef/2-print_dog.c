8ddd259b8ced28e1e2d4060ccc0d74316f8c897e
Thibaut <ThibautHolberton>
2020-10-19 11:29:44 +0000

    fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -2,37 +2,21 @@
 #include <stdio.h>
 
 /**
- * _strlen - count length of a string
- * @s: pointer char
- * Return: the length of the string given
- */
-int _strlen(char *s)
-{
-	int i, count = 0;
-
-	for (i = 0; *(s + i) != '\0'; i++)
-		count++;
-	return (count);
-}
-/**
  * print_dog - print value of this struc
  * @d: struct given
  * Return: The age, name, owner
  */
 void print_dog(struct dog *d)
 {
-	int l_name, l_owner;
 
-	l_name = _strlen(d->name);
-	l_owner = _strlen(d->owner);
 	if (d != NULL)
 	{
-		if (d->name != NULL || l_name > 0)
+		if (d->name != NULL)
 			printf("Name: %s\n", d->name);
 		else
 			printf("Name: (nil)\n");
 		printf("Age: %f\n", d->age);
-		if (d->owner != NULL || l_owner > 0)
+		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
 		else
 			printf("Owner: (nil)\n");
