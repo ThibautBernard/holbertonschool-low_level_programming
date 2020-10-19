a9af53bb6437cf52c39a48a8ec8fd97dc36ba5cf
Thibaut <ThibautHolberton>
2020-10-19 13:20:17 +0000

    fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,7 +15,27 @@ int _strlen(char *s)
 		count++;
 	return (count);
 }
+/**
+ * *_strdup - copy string str to another and return new string if success
+ * @str: string given
+ * Return: new string
+ */
+char *_strdup(char *str)
+{
+	char *n_string;
+	int i, length_str = 0;
 
+	if (str == NULL)
+		return (NULL);
+	length_str = _strlen(str);
+	n_string = malloc(length_str + 1 * sizeof(char));
+	if (n_string == NULL)
+		return (NULL);
+	for (i = 0; i < length_str; i++)
+		*(n_string + i) = *(str + i);
+	*(n_string + i) = '\0';
+	return (n_string);
+}
 /**
  * new_dog - malloc and initialize
  * @name: dog name
@@ -31,13 +51,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (newDog == NULL)
 		return (NULL);
 	if (name != NULL)
-		newDog->name = name;
-	else
-		return (NULL);
+	{
+		newDog->name = _strdup(name);
+		if (newDog->name == NULL)
+		{
+			free(newDog->name);
+			return (NULL);
+		}
+	}
 	if (owner != NULL)
-		newDog->owner = owner;
-	else
-		return (NULL);
+	{
+		newDog->owner = _strdup(owner);
+		if (newDog->owner == NULL)
+		{
+			free(newDog->owner);
+			return (NULL);
+		}
+	}
 	newDog->age = age;
 	return (newDog);
 }
