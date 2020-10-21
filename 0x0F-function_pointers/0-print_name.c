37e8dc7dc33116a38987be814901d6bfe6fd064d
Thibaut <ThibautHolberton>
2020-10-21 07:23:43 +0000

    feat(print_name && header file): create function that print a string with argument a pointer to a function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - function that print name with pointer function
+ * @name: string to print
+ * @f: pointer of function in arguments given
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
