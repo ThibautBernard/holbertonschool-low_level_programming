59c5f44bb8e07818f34db7efdf5e89a982e3700b
Thibaut <ThibautHolberton>
2020-10-23 21:11:27 +0000

    fix(): update the string if its null

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,18 +10,20 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list args;
+	char *s;
 
 	if (n != 0)
 	{
 		va_start(args, n);
 		for (i = 0; i < n; i++)
 		{
+			s = va_arg(args, char *);
 			if (separator != NULL && i + 1 != n)
-				printf("%s%s", va_arg(args, char *), separator);
-			else if (args == NULL)
+				printf("%s%s", s, separator);
+			else if (s == NULL)
 				printf("(nil)");
 			else
-				printf("%s", va_arg(args, char *));
+				printf("%s", s);
 		}
 			printf("\n");
 		va_end(args);
