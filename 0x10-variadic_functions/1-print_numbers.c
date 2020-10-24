c0258dd18d98dc364028d53083afb97ff54526d6
Thibaut <ThibautHolberton>
2020-10-24 18:43:35 +0000

    remove condition

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,18 +11,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list args;
 
-	if (n != 0)
+	va_start(args, n);
+	for (i = 0; i < n; i++)
 	{
-		va_start(args, n);
-		for (i = 0; i < n; i++)
-		{
-			if (separator != NULL && i + 1 != n)
-				printf("%d%s", va_arg(args, int), separator);
-			else
-				printf("%d", va_arg(args, int));
-		}
-		printf("\n");
-		va_end(args);
+		if (separator != NULL && i + 1 != n)
+			printf("%d%s", va_arg(args, int), separator);
+		else
+			printf("%d", va_arg(args, int));
 	}
+	printf("\n");
 	va_end(args);
 }
