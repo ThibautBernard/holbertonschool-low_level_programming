c999b74846c99c3be9cd2340d283312cea6ab44a
Thibaut <ThibautHolberton>
2020-10-24 18:45:36 +0000

    update(): remove condition n

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,21 +12,17 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list args;
 	char *s;
 
-	if (n != 0)
+	va_start(args, n);
+	for (i = 0; i < n; i++)
 	{
-		va_start(args, n);
-		for (i = 0; i < n; i++)
-		{
-			s = va_arg(args, char *);
-			if (separator != NULL && i + 1 != n)
-				printf("%s%s", s, separator);
-			else if (s == NULL)
-				printf("(nil)");
-			else
-				printf("%s", s);
-		}
-			printf("\n");
-		va_end(args);
+		s = va_arg(args, char *);
+		if (separator != NULL && i + 1 != n)
+			printf("%s%s", s, separator);
+		else if (s == NULL)
+			printf("(nil)");
+		else
+			printf("%s", s);
 	}
+	printf("\n");
 	va_end(args);
 }
