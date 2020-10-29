a83cb65fc2d1022f4ac45a3d87de65a227d87933
Thibaut <ThibautHolberton>
2020-10-29 14:24:18 +0000

    fix

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,12 +7,13 @@
  */
 void free_list(list_t *head)
 {
-		list_t *tmp = NULL;
+		list_t *tmp;
 
 		while (head != NULL)
 		{
 			tmp = head;
 			head = head->next;
+			free(tmp->str);
 			free(tmp);
 		}
 }
