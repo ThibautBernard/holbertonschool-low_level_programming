5e0d4529a13fad42cfd5c8e3fac2723f7d34fcbe
Thibaut <ThibautHolberton>
2020-10-29 14:35:06 +0000

    feat(100-first): create function that print && excute before the main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * print_before_main - function that print
+ * a string before the main function
+ * Return: nothing
+ */
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
