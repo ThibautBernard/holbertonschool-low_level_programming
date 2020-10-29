80731babd7a3026f0aaab1b331bf7516455ec25c
Thibaut <ThibautHolberton>
2020-10-29 15:19:09 +0000

    fix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -37,25 +37,51 @@ void free_list(list_t *head)
 		}
 }
 /**
+ * add_node_end - add node at the end
+ * @head: first node
+ * @str: string to duplicate
+ * Return: pointer to new last node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *next;
+
+	next = malloc(sizeof(list_t));
+	if (next == NULL)
+		return (NULL);
+	if (next != NULL)
+	{
+		next->str = strdup(str);
+		next->len = _strlen(str);
+		next->next = NULL;
+		if (*head == NULL)
+			*head = next;
+		else
+		{
+			list_t *lnode = *head;
+
+			while (lnode->next != NULL)
+			{
+				lnode = lnode->next;
+			}
+			lnode->next = next;
+		}
+	}
+	return (next);
+}
+/**
  * print_before_main - function that print
  * a string before the main function
  * Return: nothing
  */
 void print_before_main(void)
 {
-	list_t *head;
-	list_t *second;
+	list_t *head = NULL;
 	const char *f_str = "You're beat! and yet, you must allow,\n";
 	const char *s_str = "I bore my house upon my back!\n";
 
-	head = malloc(sizeof(list_t));
-	second = malloc(sizeof(list_t));
-	head->str = strdup(f_str);
-	head->next = second;
-	head->len = _strlen(f_str);
-	second->str = strdup(s_str);
-	second->next = NULL;
-	second->len = _strlen(s_str);
+	add_node_end(&head, f_str);
+	add_node_end(&head, s_str);
 
 	while (head != NULL)
 	{
