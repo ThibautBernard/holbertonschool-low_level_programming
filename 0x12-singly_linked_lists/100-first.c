4cedcdd2b9fb5f6330b5ea6ef4b4bddfe2fee5d4
Thibaut <ThibautHolberton>
2020-10-29 15:04:59 +0000

    fix(): add linked list

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,4 +1,41 @@
 #include "lists.h"
+
+
+/**
+ * _strlen - count length of string
+ * @str: string
+ * Return: length
+ */
+int _strlen(const char *str)
+{
+	int i = 0;
+
+	if (str != NULL)
+	{
+		while (str[i])
+		{
+			i++;
+		}
+	}
+	return (i);
+}
+/**
+ * free_list - free a linked list
+ * @head: first node
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+		list_t *tmp;
+
+		while (head != NULL)
+		{
+			tmp = head;
+			head = head->next;
+			free(tmp->str);
+			free(tmp);
+		}
+}
 /**
  * print_before_main - function that print
  * a string before the main function
@@ -6,7 +43,24 @@
  */
 void print_before_main(void)
 {
-	char *s;
-s = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
-	printf("%s", s);
+	list_t *head;
+	list_t *second;
+	const char *f_str = "You're beat! and yet, you must allow,\n";
+	const char *s_str = "I bore my house upon my back!\n";
+
+	head = malloc(sizeof(list_t));
+	second = malloc(sizeof(list_t));
+	head->str = strdup(f_str);
+	head->next = second;
+	head->len = _strlen(f_str);
+	second->str = strdup(s_str);
+	second->next = NULL;
+	second->len = _strlen(s_str);
+
+	while (head != NULL)
+	{
+		printf("%s", head->str);
+		head = head->next;
+	}
+	free_list(head);
 }
