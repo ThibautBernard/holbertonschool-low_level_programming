a8be21c9f90536d05c5eaaca6e9952655f1f74c8
Thibaut <ThibautHolberton>
2020-10-29 19:03:19 +0000

    fix

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,18 @@
+extern	printf
 
-global _start
+section .data
+msg:	db "Hello Holberton", 0
+fmt: db "%s", 10, 0 
 
 section .text
+global main
+main:
+  push rbp
+	
+	mov	rdi,fmt
+	mov	rsi,msg
+  call printf
 
-_start:
-  mov rax, 1        ; write(
-  mov rdi, 1        ;   STDOUT_FILENO,
-  mov rsi, msg      ;   "Hello, world!\n",
-  mov rdx, msglen   ;   sizeof("Hello, world!\n")
-  syscall           ; );
-
-  mov rax, 60       ; exit(
-  mov rdi, 0        ;   EXIT_SUCCESS
-  syscall           ; );
-
-section .rodata
-  msg: db "Hello, Holberton", 10
-  msglen: equ $ - msg
+	pop rbp
+	mov	rax,0
+	ret
