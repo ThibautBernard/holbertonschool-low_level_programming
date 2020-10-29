389513077f6e205e50152340bd19d3d4b6856cb7
Thibaut <ThibautHolberton>
2020-10-29 18:42:06 +0000

    fix

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,19 @@
-section .data
-msg db "Hello, Holberton", 10
-msglen eq $ - msg
+
+global _start
 
 section .text
-global _start
+
 _start:
+  mov rax, 1        ; write(
+  mov rdi, 1        ;   STDOUT_FILENO,
+  mov rsi, msg      ;   "Hello, world!\n",
+  mov rdx, msglen   ;   sizeof("Hello, world!\n")
+  syscall           ; );
 
-	mov rax, 1			;use the write syscall
-  mov rdi, 1
-	mov rsi, msg
-	mov rdx, msglen
-	syscall
+  mov rax, 60       ; exit(
+  mov rdi, 0        ;   EXIT_SUCCESS
+  syscall           ; );
 
-	mov rax, 60
-	mov rdi, 0
-	syscall
-
+section .rodata
+  msg: db "Hello, Holberton", 10
+  msglen: equ $ - msg
