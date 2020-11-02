3436a97363dab6b945697afb9c97488777d30cf0
Thibaut <ThibautHolberton>
2020-11-02 09:16:52 +0000

    feat(free_listint2 && lists): create function that freed a linked list and add null to the pointer of pointer && update lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - free a linked list of int and add null
+ * @head: pointer of pointer that contain adress of
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp, *store = *head;
+
+	while (store != NULL)
+	{
+		tmp = store;
+		store = store->next;
+		free(tmp);
+	}
+	*head = NULL;
+}
