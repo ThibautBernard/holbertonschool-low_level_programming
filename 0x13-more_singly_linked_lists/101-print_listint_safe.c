b3b9b44544464ab68b1e8aa6cb99d8b330a7eefc
Thibaut <ThibautHolberton>
2020-11-03 13:27:09 +0000

    feat(print_listint_safe && lists)

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_listint_safe - print a list
+ * @head: linked list
+ * Return: nodes
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *fast = head, *slow = head;
+	int i = 0;
+
+	if (head == NULL)
+		exit(98);
+	printf("[%p] %d\n", slow->next, slow->n);
+	while (slow->next != NULL)
+	{
+		slow = slow->next;
+		if (fast->next->next != NULL)
+			fast = fast->next->next;
+		i++;
+		printf("[%p] %d\n", slow, slow->n);
+		if (slow == fast && fast->next->next != NULL)
+		{
+			i++;
+			slow = slow->next;
+			fast = fast->next->next;
+			printf("[%p] %d\n", slow, slow->n);
+			printf("-> [%p] %d\n", fast, fast->n);
+			break;
+		}
+	}
+	return (i);
+}
