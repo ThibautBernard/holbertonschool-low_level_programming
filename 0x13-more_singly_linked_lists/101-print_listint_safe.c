4b67b1670a449cfb3ba4e82d8953d204b442285e
Thibaut <ThibautHolberton>
2020-11-03 13:29:16 +0000

    feat(print_listint_safe && lists)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -12,21 +12,21 @@ size_t print_listint_safe(const listint_t *head)
 
 	if (head == NULL)
 		exit(98);
-	printf("[%p] %d\n", slow->next, slow->n);
+	printf("[%p] %d\n", (void*)slow->next, slow->n);
 	while (slow->next != NULL)
 	{
 		slow = slow->next;
 		if (fast->next->next != NULL)
 			fast = fast->next->next;
 		i++;
-		printf("[%p] %d\n", slow, slow->n);
+		printf("[%p] %d\n", (void*)slow, slow->n);
 		if (slow == fast && fast->next->next != NULL)
 		{
 			i++;
 			slow = slow->next;
 			fast = fast->next->next;
-			printf("[%p] %d\n", slow, slow->n);
-			printf("-> [%p] %d\n", fast, fast->n);
+			printf("[%p] %d\n", (void*)slow, slow->n);
+			printf("-> [%p] %d\n", (void*)fast, fast->n);
 			break;
 		}
 	}
