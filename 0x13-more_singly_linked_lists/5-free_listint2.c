c9545e27f8d921184796a972065033f888b13983
Thibaut <ThibautHolberton>
2020-11-03 18:39:49 +0000

    fix()

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 /**
  * free_listint2 - free a linked list of int and add null
  * @head: pointer of pointer that contain adress of
@@ -6,14 +7,18 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp, *store = *head;
-
-	if ((*head)->next != NULL)
-		*head = NULL;
-	while (store != NULL)
+	if (head != NULL)
 	{
-		tmp = store;
-		store = store->next;
-		free(tmp);
+		listint_t *tmp, *store = *head;
+
+		while (store != NULL)
+		{
+			tmp = store;
+			store = store->next;
+			free(tmp);
+		}
+		*head = NULL;
 	}
+	else
+		return;
 }
