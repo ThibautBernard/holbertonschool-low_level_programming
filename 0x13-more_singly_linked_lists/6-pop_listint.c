47ebe8186bd400c24d5b9d0b19054a5f21edc846
Thibaut <ThibautHolberton>
2020-11-03 08:39:12 +0000

    fix

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,10 +8,11 @@ int pop_listint(listint_t **head)
 {
 	int tmp_nb;
 	listint_t *to_free = *head;
+
 	if (*head == NULL)
 		return (0);
-	*head = (*head)->next;
 	tmp_nb = to_free->n;
+	*head = (*head)->next;
 	free(to_free);
 	return (tmp_nb);
 }
