59b755e94cc4ff03784088774dbc5361f32114e6
Thibaut <ThibautHolberton>
2020-11-05 11:20:27 +0000

    fix

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -30,7 +30,5 @@ int get_bit(unsigned long int n, unsigned int index)
 		counter++;
 		i++;
 	}
-	if (index >= counter)
-		return (-1);
-	return ((n >> index) & 1);
+	return (((n >> index) & 1));
 }
