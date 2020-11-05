d11f06e49f8f0ca918cae5223ec77cc0e1e9cfd7
Thibaut <ThibautHolberton>
2020-11-05 12:03:29 +0000

    feat(set_bit && holberton)

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * set_bit - set 1 to the index of n
+ * @n: unsigned int
+ * @index: the index of bit
+ * Return: new number or -1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+	return (*n |= (1 << index));
+}
