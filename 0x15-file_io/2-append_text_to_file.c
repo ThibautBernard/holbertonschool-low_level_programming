81432f468b04783e6d4b5ab5eb95c62e74141f86
Thibaut <thibautbernard@sfr.fr>
2020-11-09 13:24:33 +0000

    feat(append_text && holberton)

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * append_text_to_file - append a file
+ * @filename: name of the file
+ * @text_content: content to add in the file
+ * Return: 1 if success, -1 error
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int r_write = 0, fdt = 0;
+
+	if (filename == NULL || text_content == NULL)
+		return (-1);
+	fdt = open(filename, O_WRONLY | O_APPEND);
+	if (fdt == -1)
+		return (-1);
+	r_write = write(fdt, text_content, strlen(text_content));
+	if (return_write == -1)
+		return (-1);
+	return (1);
+}
