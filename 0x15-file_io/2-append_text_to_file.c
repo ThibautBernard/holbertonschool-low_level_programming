3932f802596d8669cf19b2af91d65a55e389722a
Thibaut <thibautbernard@sfr.fr>
2020-11-09 13:25:36 +0000

    feat(append_text && holberton)

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,7 +17,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fdt == -1)
 		return (-1);
 	r_write = write(fdt, text_content, strlen(text_content));
-	if (return_write == -1)
+	if (r_write == -1)
 		return (-1);
 	return (1);
 }
