2dd6d1ef79862276a6278ca5c91614510355afc3
Thibaut <thibautbernard@sfr.fr>
2020-11-09 09:14:28 +0000

    new project

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,8 @@
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+#include <stdio.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#endif
