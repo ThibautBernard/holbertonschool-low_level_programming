068236f2a7cbb59b95793c675c791109edd5f8f5
Thibaut <thibautbernard@sfr.fr>
2020-11-09 12:07:01 +0000

    feat(create_file && holberton)

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -2,6 +2,7 @@
 #define _HOLBERTON_H
 #include <stdio.h>
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
