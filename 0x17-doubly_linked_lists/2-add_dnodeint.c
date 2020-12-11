7029266783fe2a69823590822cb50443cc5f62c5
Thibaut <thibautbernard@sfr.fr>
2020-12-11 13:51:10 +0000

    feat()

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * add_dnodeint - add a new node at the start
+ * @head: linked list adress
+ * @n: number to add
+ * Return: number of nodes
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
