6edee548c28199d7ba68b3000eedcf3380c957fc
Thibaut <thibautbernard@sfr.fr>
2020-12-13 11:04:58 +0000

    fix

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,11 +12,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-	if (head == NULL)
-		return (NULL);
 	new->n = n;
 	new->prev = NULL;
-	new->next = *head;
+	if (*head == NULL)
+		new->next = NULL;
+	else
+		new->next = *head;
 	*head = new;
 	return (new);
 }
