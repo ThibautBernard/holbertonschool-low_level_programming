c1bafe6618f1b556decb8f27d0f127821ea37a2a
Thibaut <thibautbernard@sfr.fr>
2020-12-13 11:53:04 +0000

    fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,8 +18,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		new->n = n;
 		new->prev = NULL;
-		new->next = current->next;
-		current->prev = new;
+		new->next = NULL;
 		*h = new;
 		return (new);
 	}
