53b3b5615970ea67e05d4df726fe0b13f017da11
Thibaut <thibautbernard@sfr.fr>
2021-02-19 16:06:28 +0000

    fix(): edges

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -14,15 +14,22 @@ def island_perimeter(grid):
     idx_v = None
     counter_vert = 0
     counter_h = 0
-    signal = 0
+    len_width = len(grid[0])
+    l_h = len(grid)
     for idx_h, h in enumerate(grid):
         for idx_w, w in enumerate(h):
-            if w == 1 and grid[idx_h + 1][idx_w] == 1 or w == 1 \
-             and grid[idx_h + 1][idx_w] == 0 and\
-             grid[idx_h - 1][idx_w] == 1 or w == 1 and counter_vert == 0:
+            if idx_h + 1 < l_h and idx_h - 1 >= 0:
+                if w == 1 and grid[idx_h + 1][idx_w] == 1 or w == 1 \
+                 and grid[idx_h + 1][idx_w] == 0 and\
+                 grid[idx_h - 1][idx_w] == 1 or w == 1 and counter_vert == 0:
+                    counter_vert += 1
+            elif counter_vert == 0 and w == 1 or idx_h + 1 >= l_h and w == 1:
                 counter_vert += 1
-            if w == 1 and h[idx_w - 1] == 1 or w == 1 and counter_h == 0:
+            if idx_w - 1 >= 0:
+                if w == 1 and h[idx_w - 1] == 1 or w == 1 and counter_h == 0:
+                    counter_h += 1
+            elif counter_h == 0 and w == 1:
                 counter_h += 1
-    # print("vertical : {}".format(counter_vert))
-    # print("horizontal : {}".format(counter_h))
+    # print("Vertical  : {}".format(counter_vert))
+    # print("Horizontal : {}".format(counter_h))
     return (2 * (counter_vert + counter_h))
