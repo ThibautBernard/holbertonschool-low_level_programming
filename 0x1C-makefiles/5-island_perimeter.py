4df20de5584bc3a3cb7fc83535efff45f40817fa
Thibaut <thibautbernard@sfr.fr>
2021-02-19 15:19:10 +0000

    fix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -14,13 +14,14 @@ def island_perimeter(grid):
     idx_v = None
     counter_vert = 0
     counter_h = 0
+    signal = 0
     for idx_h, h in enumerate(grid):
         for idx_w, w in enumerate(h):
-            if w == 1 and grid[idx_h - 1][idx_w] == 1 \
-                                    or w == 1 and grid[idx_h + 1][idx_w] == 1:
+            if w == 1 and grid[idx_h + 1][idx_w] == 1 or w == 1 \
+             and grid[idx_h + 1][idx_w] == 0 and grid[idx_h - 1][idx_w] == 1:
                 counter_vert += 1
             if w == 1 and h[idx_w + 1] == 1 or w == 1 and h[idx_w - 1] == 1:
                 counter_h += 1
     # print("vertical : {}".format(counter_vert))
     # print("horizontal : {}".format(counter_h))
-    return (counter_vert * 2) + (counter_h * 2)
+    return (2 * (counter_vert + counter_h))
