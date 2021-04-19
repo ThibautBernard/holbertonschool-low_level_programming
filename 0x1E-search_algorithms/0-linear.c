503e31b6bb8aab3f4522f74f0e9d75eeed115e13
ThibautBernard <thibautbernard@sfr.fr>
2021-04-19 10:16:52 +0000

    feat

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+/**
+ * linear_search - check in array a value and return index
+ * @array: array to check
+ * @size: size of the array
+ * @value: value to find
+ * Return: The index if success or -1 if not
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+	char *s = "Value checked array[%ld] = [%d]\n";
+
+	for (; i < size; i++)
+	{
+		if (array[i] == value)
+		{
+			printf(s, i, array[i]);
+			return (i);
+		}
+		else
+			printf(s, i, array[i]);
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,7 @@
+#ifndef _SEARCH_ALGORITHMS_
+#define _SEARCH_ALGORITHMS_
+#include <stdio.h>
+#include <stddef.h>
+int linear_search(int *array, size_t size, int value);
+
+#endif
