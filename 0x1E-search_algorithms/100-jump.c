acd718aa979b945bd3aa51a9b470a3a83473be3d
ThibautBernard <thibautbernard@sfr.fr>
2021-04-20 12:30:30 +0000

    fix

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -14,7 +14,7 @@ int jump_search(int *array, size_t size, int value)
 	size_t jump = sqrt(size), b_jump = 0;
 	size_t x;
 
-	if (array == NULL)
+	if (array == NULL || size <= 0)
 		return (-1);
 	printf("Value checked array[%ld] = [%d]\n", b_jump, array[b_jump]);
 	while (array[jump] < value && jump < size)
